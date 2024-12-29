d82555cc7b103520f8c0d008e669209b5091ff0e
zhuchangjun <zhuchangjun@hotmail.com>
2024-12-29 14:01:37 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>,
+)
